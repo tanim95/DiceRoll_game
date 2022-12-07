fa5b49fa7fd686e41ad1e43be22af06e58cf69aa
tanim95 <tanimrahman78@gmail.com>
2022-12-07 22:44:33 +0600

    commit 3: Pig Game fully functional

--- script.js
+++ script.js
@@ -13,7 +13,89 @@ const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
 
+// Global Var
+let score, playing, activePlayer, currentScore;
+playing = true;
+score = [0, 0];
+currentScore = 0;
+activePlayer = 0;
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
+
+const init = function () {
+  score = [0, 0];
+  playing = true;
+  currentScore = 0;
+  score0El.textContent = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+  diceEl.classList.add('hidden');
+  // player0El.classList.remove('palyer--winner');
+  // player1El.classList.remove('palyer--winner');
+  // player1El.classList.remove('palyer--active');
+  // player1El.classList.remove('palyer--active');
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--winner');
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.add('player--active');
+  document.getElementById(
+    `name--${activePlayer}`
+  ).textContent = `PLAYER ${activePlayer}`;
+};
+
 //Starting condition
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+//Rolling dice Functionality
+btnRoll.addEventListener('click', () => {
+  if (playing) {
+    //Genarating random number
+    const dice = Math.trunc(Math.random() * 6 + 1);
+    //Display dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${dice}.png`;
+
+    //Check for rolled 1:if true switch to next player
+    if (dice !== 1) {
+      currentScore += dice;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    score[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      score[activePlayer];
+    // Check if the score is 50
+    if (score[activePlayer] >= 50) {
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+      document.getElementById(`name--${activePlayer}`).textContent = 'WINNER';
+      playing = false;
+    } else {
+      switchPlayer();
+    }
+  }
+});
+
+//New game
+btnNew.addEventListener('click', init);
